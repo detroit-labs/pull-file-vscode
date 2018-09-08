672513db034ec6458c5617f48cf9228009042cf3
Shawn Mallon <shawn.mallon@detroitlabs.com>
2018-09-08 18:12:45 -0400

    Initial commit of Pull File to github.

---
+++ README.md
@@ -0,0 +1,33 @@
+# pull-file README
+
+Pull File is a VS Code extension that is used to overwrite the current file with a selected file.
+
+## Features
+
+Using the command Pull File, you can select a file to overwrite the current file with.
+
+When opening the Open File Dialog, Pull File will add an extension filter for the current file extension.
+
+If the currently opened editor has changes that are unsaved then Pull File will save them just before overwriting, so VS Code will reflect the external changes.
+
+## Requirements
+
+Pull File does not have any additional requirements beyond VS Code.
+
+## Extension Settings
+
+No settings are added by Pull File.
+
+## Known Issues
+
+When selecting a file with a different extension than the current one, Pull File saves the selected file with the current file's extension.
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of Pull File
+
+### 1.0.1
+
+Change Open File Dialog open label from Pull Content to Pull File.
---
+++ src/extension.ts
@@ -0,0 +1,96 @@
+'use strict';
+import { window, ExtensionContext, commands, TextDocument, TextEditor, OpenDialogOptions } from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+export function activate(context: ExtensionContext) {
+    let disposable = commands.registerCommand('extension.pullFile', () => {
+        // Pull content from a file into the active file.
+        let editor = window.activeTextEditor;
+        // Verify the active TextEditor is not undefined.
+        if (isTextEditor(editor)) {
+            let pullFile = new PullFile(editor);
+            pullFile.Pull();
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
+/**
+ * Pull the contents of a file into the active file.
+ */
+class PullFile {
+    _activeDocument: TextDocument;
+    _extension: string;
+
+    /**
+     * Get the active TextDocument and its file extension from the active TextEditor.
+     * @param editor The active TextEditor
+     */
+    constructor(editor: TextEditor) {
+        this._activeDocument = editor.document;
+        this._extension = path.extname(this._activeDocument.fileName).replace('.', '');
+    }
+
+    /**
+     * Pull the contents of a file into the active file.
+     */
+    Pull() {
+        const options = this.GetOptions();
+    
+        // Let the user pick a file with the OS file open dialog.
+        window.showOpenDialog(options).then(fileUri => {
+            // Once the dialog has been closed, make sure there is a file picked.
+            if (fileUri && fileUri[0]) {
+                // If the file is dirty, then save it before overwriting so the editor shows the change.
+                if (this._activeDocument.isDirty) {
+                    this._activeDocument.save().then(() => {
+                        // Copy the selected file to the currently opened file and overwrite it.
+                        fs.copyFileSync(fileUri[0].fsPath, this._activeDocument.fileName);
+                    });
+                }
+                else { // If the file isn't dirty then we can just overwrite and the editor will show the change.
+                    // Copy the selected file to the currently opened file and overwrite it.
+                    fs.copyFileSync(fileUri[0].fsPath, this._activeDocument.fileName);
+                }
+            }
+        });
+    }
+
+    /**
+     * Get the OpenDialogOptions for the OpenDialog.
+     */
+    private GetOptions(): OpenDialogOptions {
+        // Set the options for the OpenDialog.
+        const options: OpenDialogOptions = {
+            canSelectMany: false,
+            openLabel: "Pull File",
+            filters: {
+                "All Files": ["*"]
+            }
+        };
+    
+        // Add a filter for the current file type if the extension can be found.
+        if (this._extension !== "") {
+            options.filters = {
+                "Current File Type": [this._extension],
+                "All Files": ["*"]
+            };
+        }
+
+        return options;
+    }
+}
+
+/**
+ * Type guard to verify editor is a TextEditor.
+ * @param editor The editor to type guard.
+ */
+function isTextEditor(editor: any): editor is TextEditor {
+    return editor !== undefined && (<TextEditor>editor).document !== undefined;
+}
